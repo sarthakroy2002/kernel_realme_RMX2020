c385b355fcf9d27f865870879296d65b66f89bb9
Sukrut4778 <sukrutbhagwat4778@gmail.com>
2020-07-21 11:24:47 +0530

    proc: Remove realme bootverification from /proc/cmdline
    Signed-off-by: Sukrut4778 <sukrutbhagwat4778@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -45,6 +45,7 @@ static void remove_safetynet_flags(char *cmd)
 	remove_flag(cmd, "androidboot.secboot=");
 	remove_flag(cmd, "androidboot.verifiedbootstate=");
 	remove_flag(cmd, "androidboot.veritymode=");
+	remove_flag(cmd, "androidboot.realmebootstate=");
 }
 
 static int __init proc_cmdline_init(void)
