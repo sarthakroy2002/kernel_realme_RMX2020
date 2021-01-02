c3eccf4c1f691f48a6712deb6501ae534b27182a
Erfan Abdi <erfangplus@gmail.com>
2021-01-02 20:27:54 +0330

    arm64: dts: mediatek: Fix dtbo building
    Change-Id: I8b3b56630bfd3fdc5410dd1a22dbf5584399ff18

--- scripts/drvgen/drvgen.mk
+++ scripts/drvgen/drvgen.mk
@@ -25,11 +25,11 @@ PROJ_DT_NAMES := $(subst $\",,$(CONFIG_BUILD_ARM_DTB_OVERLAY_IMAGE_NAMES))
 endif
 
 MAIN_DTB_NAMES := $(addsuffix .dtb,$(MAIN_DT_NAMES))
-PROJ_DTB_NAMES := $(addsuffix .dtb,$(PROJ_DT_NAMES))
+PROJ_DTB_NAMES := $(addsuffix .dtbo,$(PROJ_DT_NAMES))
 MAIN_DTB_FILES := $(addprefix $(objtree)/arch/$(SRCARCH)/boot/dts/, $(MAIN_DTB_NAMES))
 PROJ_DTB_FILES := $(addprefix $(objtree)/arch/$(SRCARCH)/boot/dts/, $(PROJ_DTB_NAMES))
 PROJ_DTS_FILES := $(addsuffix .dts,$(addprefix $(srctree)/arch/$(SRCARCH)/boot/dts/, $(PROJ_DT_NAMES)))
-ABS_DTB_FILES := $(abspath $(addsuffix .dtb,$(addprefix $(objtree)/arch/$(SRCARCH)/boot/dts/,$(PROJ_DT_NAMES))))
+ABS_DTB_FILES := $(abspath $(addsuffix .dtbo,$(addprefix $(objtree)/arch/$(SRCARCH)/boot/dts/,$(PROJ_DT_NAMES))))
 ABS_DTB2_FILES := $(abspath $(addprefix $(objtree)/arch/$(SRCARCH)/boot/,mtk.dtb))
 
 export PROJ_DTB_FILES
@@ -65,11 +65,6 @@ $(DRVGEN_FILE_LIST): $(DRVGEN_TOOL) $(DWS_FILE) $(DRVGEN_FIG) $(PROJ_DTS_FILES)
 			mkdir -p $$prj_path ;\
 			$(python) $(DRVGEN_TOOL) $$dws_path $$prj_path $$prj_path cust_dtsi;\
 		fi \
-	done
-
-dtbo_check: $(MAIN_DTB_NAMES) $(PROJ_DTB_NAMES)
-	for i in $(PROJ_DTB_FILES); do \
-		$(srctree)/scripts/dtc/ufdt_apply_overlay $(MAIN_DTB_FILES) $$i $$i.merge;\
 	done
 
 my_dtbo_id := 0
