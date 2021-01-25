eaa226be93c2b992028bd660a61185b2220c99dd
Alessio Balsini <balsini@google.com>
2021-01-25 17:03:31 +0000

    FROMLIST: fuse: Introduce synchronous read and write for passthrough
    All the read and write operations performed on fuse_files which have the
    passthrough feature enabled are forwarded to the associated lower file
    system file via VFS.
    
    Sending the request directly to the lower file system avoids the
    userspace round-trip that, because of possible context switches and
    additional operations might reduce the overall performance, especially
    in those cases where caching doesn't help, for example in reads at
    random offsets.
    
    Verifying if a fuse_file has a lower file system file associated with
    can be done by checking the validity of its passthrough_filp pointer.
    This pointer is not NULL only if passthrough has been successfully
    enabled via the appropriate ioctl().
    When a read/write operation is requested for a FUSE file with
    passthrough enabled, a new equivalent VFS request is generated, which
    instead targets the lower file system file.
    The VFS layer performs additional checks that allow for safer operations
    but may cause the operation to fail if the process accessing the FUSE
    file system does not have access to the lower file system.
    
    This change only implements synchronous requests in passthrough,
    returning an error in the case of asynchronous operations, yet covering
    the majority of the use cases.
    
    Bug: 179164095
    Link: https://lore.kernel.org/lkml/20210125153057.3623715-6-balsini@android.com/
    Signed-off-by: Alessio Balsini <balsini@android.com>
    Change-Id: Ifbe6a247fe7338f87d078fde923f0252eeaeb668
    Signed-off-by: Alessio Balsini <balsini@google.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -986,12 +986,12 @@ static ssize_t fuse_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 {
 	struct inode *inode = iocb->ki_filp->f_mapping->host;
 	struct fuse_conn *fc = get_fuse_conn(inode);
+	struct fuse_file *ff = iocb->ki_filp->private_data;
 #ifdef CONFIG_OPLUS_FEATURE_FUSE_FS_SHORTCIRCUIT
 //shubin@BSP.Kernel.FS 2020/08/20 improving fuse storage performance
-	struct fuse_file *ff = iocb->ki_filp->private_data;
 	ssize_t ret_val;
 #endif /* CONFIG_OPLUS_FEATURE_FUSE_FS_SHORTCIRCUIT */
-
+	
 	if (fuse_is_bad(inode))
 		return -EIO;
 
@@ -1007,6 +1007,8 @@ static ssize_t fuse_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 		if (err)
 			return err;
 	}
+	if (ff->passthrough.filp)
+		return fuse_passthrough_read_iter(iocb, to);
 #ifdef CONFIG_OPLUS_FEATURE_FUSE_FS_SHORTCIRCUIT
 //shubin@BSP.Kernel.FS 2020/08/20 improving fuse storage performance
 	if (ff && ff->rw_lower_file)
@@ -1264,6 +1266,10 @@ static ssize_t fuse_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	struct inode *inode = mapping->host;
 	ssize_t err;
 	loff_t endbyte = 0;
+	struct fuse_file *ff = file->private_data;
+
+	if (ff->passthrough.filp)
+		return fuse_passthrough_write_iter(iocb, from);
 
 #ifdef CONFIG_OPLUS_FEATURE_FUSE_FS_SHORTCIRCUIT
 //shubin@BSP.Kernel.FS 2020/08/20 improving fuse storage performance
