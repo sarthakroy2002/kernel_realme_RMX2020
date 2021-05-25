b879d6dd4ef7d9f67ccb418b0239febf5df14c7e
TheMalachite <eliasgheeraert@gmail.com>
2021-05-25 14:52:09 +0200

    treewide: Nuke OPPO Phoenix drivers
    Change-Id: I50a956b29987b49ae146bb6eb1a483c906b939c0

--- init/main.c
+++ init/main.c
@@ -99,12 +99,6 @@
 #include <mt-plat/mtk_ram_console.h>
 #endif
 
-#ifdef VENDOR_EDIT
-// Kun.Hu@TECH.BSP.Stability.PHOENIX_PROJECT 2019/06/11, Add for phoenix project
-#include "../drivers/soc/oppo/oppo_phoenix/oppo_phoenix.h"
-#endif  //VENDOR_EDIT
-
-
 static int kernel_init(void *);
 
 extern void init_IRQ(void);
@@ -582,12 +576,6 @@ asmlinkage __visible void __init start_kernel(void)
 	sort_main_extable();
 	trap_init();
 	mm_init();
-
-#ifdef VENDOR_EDIT
-	// Kun.Hu@PSW.TECH.RELIABILTY, 2018/11/15, add for project phoenix(hang oppo)
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_MM_INIT_DONE);
-#endif
 	ftrace_init();
 
 	/* trace_printk can be enabled here */
@@ -640,12 +628,6 @@ asmlinkage __visible void __init start_kernel(void)
 	WARN(!irqs_disabled(), "Interrupts were enabled early\n");
 	early_boot_irqs_disabled = false;
 	local_irq_enable();
-
-#ifdef VENDOR_EDIT
-    // Kun.Hu@TECH.BSP.Stability.PHOENIX_PROJECT 2019/06/11, Add for phoenix project
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_LOCAL_IRQ_ENABLE);
-#endif
 	kmem_cache_init_late();
 
 	/*
@@ -715,12 +697,6 @@ asmlinkage __visible void __init start_kernel(void)
 	cgroup_init();
 	taskstats_init_early();
 	delayacct_init();
-
-#ifdef VENDOR_EDIT
-    // Kun.Hu@PSW.TECH.RELIABILTY, 2018/11/15, add for project phoenix(hang oppo)
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_DELAYACCT_INIT_DONE);
-#endif
 	check_bugs();
 
 	acpi_subsystem_init();
@@ -969,20 +945,10 @@ static void __init do_basic_setup(void)
 	cpuset_init_smp();
 	shmem_init();
 	driver_init();
-#ifdef VENDOR_EDIT
-    // Kun.Hu@TECH.BSP.Stability.PHOENIX_PROJECT 2019/06/11, Add for phoenix project
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_DRIVER_INIT_DONE);
-#endif
 	init_irq_proc();
 	do_ctors();
 	usermodehelper_enable();
 	do_initcalls();
-#ifdef VENDOR_EDIT
-    // Kun.Hu@TECH.BSP.Stability.PHOENIX_PROJECT 2019/06/11, Add for phoenix project
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_DO_INITCALLS_DONE);
-#endif
 }
 
 static void __init do_pre_smp_initcalls(void)
@@ -1079,11 +1045,6 @@ static int __ref kernel_init(void *unused)
 #ifdef CONFIG_MTPROF
 		log_boot("Kernel_init_done");
 #endif
-#ifdef VENDOR_EDIT
-        // Kun.Hu@TECH.BSP.Stability.PHOENIX_PROJECT 2019/06/11, Add for phoenix project
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_INIT_DONE);
-#endif
 	if (ramdisk_execute_command) {
 		ret = run_init_process(ramdisk_execute_command);
 		if (!ret)
@@ -1150,11 +1111,6 @@ static noinline void __init kernel_init_freeable(void)
 
 	do_basic_setup();
 
-#ifdef VENDOR_EDIT
-    // Kun.Hu@TECH.BSP.Stability.PHOENIX_PROJECT 2019/06/11, Add for phoenix project
-	if(phx_set_boot_stage)
-		phx_set_boot_stage(KERNEL_DO_BASIC_SETUP_DONE);
-#endif
 	/* Open the /dev/console on the rootfs, this should never fail */
 	if (sys_open((const char __user *) "/dev/console", O_RDWR, 0) < 0)
 		pr_err("Warning: unable to open an initial console.\n");
--- kernel/panic.c
+++ kernel/panic.c
@@ -35,11 +35,6 @@
 #define PANIC_BLINK_SPD 18
 
 #ifdef VENDOR_EDIT
-// Kun.Hu@TECH.BSP.Stability.PHOENIX_PROJECT 2019/06/11, Add for phoenix project
-#include "../drivers/soc/oppo/oppo_phoenix/oppo_phoenix.h"
-static int kernel_panic_happened = 0;
-#endif
-#ifdef VENDOR_EDIT
 /* Bin.Li@EXP.BSP.bootloader.bootflow, 2017/05/24, Add for interface reboot reason */
 int is_kernel_panic = 0;
 #endif
@@ -171,18 +166,6 @@ void panic(const char *fmt, ...)
 	int old_cpu, this_cpu;
 	bool _crash_kexec_post_notifiers = crash_kexec_post_notifiers;
 
-#ifdef VENDOR_EDIT
-    // Kun.Hu@TECH.BSP.Stability.PHOENIX_PROJECT 2019/06/11, Add for phoenix project
-    kernel_panic_happened++;
-	if(phx_set_boot_error && phx_is_phoenix_boot_completed)
-	{
-		// we only care about panic on boot not complete
-		if(kernel_panic_happened < 2 && !phx_is_phoenix_boot_completed())
-		{
-			phx_set_boot_error(ERROR_KERNEL_PANIC);
-		}
-	}
-#endif  /*VENDOR_EDIT*/
 	/*
 	 * Disable local interrupts. This will prevent panic_smp_self_stop
 	 * from deadlocking the first cpu that invokes the panic, since
