baa86884668f774fff41f15840b6ee979ce9c5b7
iamlordutkarsh <lalbaake456@gmail.com>
2021-06-13 22:27:10 +0530

    block: Import Realme C3 Q-OSS changes

--- block/blk-flush.c
+++ block/blk-flush.c
@@ -92,7 +92,10 @@ enum {
 	 */
 	FLUSH_PENDING_TIMEOUT	= 5 * HZ,
 };
-
+#ifdef VENDOR_EDIT
+/*jason.tang@TECH.BSP.Kernel.Storage, 2019-05-20, add to count flush*/
+extern unsigned long sysctl_blkdev_issue_flush_count;
+#endif
 static bool blk_kick_flush(struct request_queue *q,
 			   struct blk_flush_queue *fq);
 
@@ -138,10 +141,22 @@ static bool blk_flush_queue_rq(struct request *rq, bool add_front)
 		blk_mq_add_to_requeue_list(rq, add_front, true);
 		return false;
 	} else {
+#ifdef VENDOR_EDIT
+/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
+		if (add_front) {
+			list_add(&rq->queuelist, &rq->q->queue_head);
+			queue_throtl_add_request(rq->q, rq, true);
+		}
+		else {
+			list_add_tail(&rq->queuelist, &rq->q->queue_head);
+			queue_throtl_add_request(rq->q, rq, false);
+		}
+#else
 		if (add_front)
 			list_add(&rq->queuelist, &rq->q->queue_head);
 		else
 			list_add_tail(&rq->queuelist, &rq->q->queue_head);
+#endif /*VENDOR_EDIT*/
 		return true;
 	}
 }
@@ -465,7 +480,15 @@ void blk_insert_flush(struct request *rq)
 		if (q->mq_ops)
 			blk_mq_sched_insert_request(rq, false, true, false, false);
 		else
+#ifdef VENDOR_EDIT
+/*Huacai.Zhou@PSW.BSP.Kernel.Performance, 2018-04-28, add foreground task io opt*/
+		{
 			list_add_tail(&rq->queuelist, &q->queue_head);
+			queue_throtl_add_request(q, rq, false);
+		}
+#else
+			list_add_tail(&rq->queuelist, &q->queue_head);
+#endif /*VENDOR_EDIT*/
 		return;
 	}
 
@@ -523,6 +546,10 @@ int blkdev_issue_flush(struct block_device *bdev, gfp_t gfp_mask,
 	 */
 	if (!q->make_request_fn)
 		return -ENXIO;
+#ifdef VENDOR_EDIT
+	/*jason.tang@TECH.BSP.Kernel.Storage, 2019-05-20, add to count flush*/
+	sysctl_blkdev_issue_flush_count++;
+#endif
 
 	bio = bio_alloc(gfp_mask, 0);
 	bio_set_dev(bio, bdev);
