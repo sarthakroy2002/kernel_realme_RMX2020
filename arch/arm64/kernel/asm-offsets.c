2924dd1909d20af0a28fab1d23a9ec036fa4688f
iamlordutkarsh <lalbaake456@gmail.com>
2021-06-13 23:04:11 +0530

    arm64: kernel: Import Realme C3 Q-OSS changes

--- arch/arm64/kernel/asm-offsets.c
+++ arch/arm64/kernel/asm-offsets.c
@@ -162,5 +162,20 @@ int main(void)
 #ifdef CONFIG_UNMAP_KERNEL_AT_EL0
   DEFINE(TRAMP_VALIAS,		TRAMP_VALIAS);
 #endif
+#ifdef VENDOR_EDIT
+//Ke.Li@ROM.Security Add for prevent root check
+#ifdef CONFIG_OPPO_ROOT_CHECK
+  DEFINE(PROOT_TSK_CRED,	offsetof(struct task_struct, cred));
+  DEFINE(PROOT_CRED_UID,	offsetof(struct cred, uid));
+  DEFINE(PROOT_CRED_EUID,	offsetof(struct cred, euid));
+  DEFINE(PROOT_CRED_FSUID,	offsetof(struct cred, fsuid));
+#ifdef CONFIG_THREAD_INFO_IN_TASK
+  DEFINE(PROOT_THREAD_ADDR_LIMIT,	offsetof(struct task_struct, thread_info.addr_limit));
+#else
+  DEFINE(PROOT_THREAD_TSK,	offsetof(struct thread_info,task));
+  DEFINE(PROOT_THREAD_ADDR_LIMIT,	offsetof(struct thread_info, addr_limit));
+#endif
+#endif /* CONFIG_OPPO_ROOT_CHECK */
+#endif /* VENDOR_EDIT */
   return 0;
 }
