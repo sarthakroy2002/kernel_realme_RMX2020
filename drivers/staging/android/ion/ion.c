ad7c639f176cc4261e49d6d17818e16508f26847
iamlordutkarsh <lalbaake456@gmail.com>
2021-06-13 22:30:54 +0530

    drivers: staging: Import Realme C3 Q-OSS changes

--- drivers/staging/android/ion/ion.c
+++ drivers/staging/android/ion/ion.c
@@ -47,6 +47,12 @@
 #include "mtk/mtk_ion.h"
 #include "mtk/ion_drv_priv.h"
 
+#ifdef VENDOR_EDIT
+// wenbin.liu@PSW.BSP.MM, 2018/07/11
+// Add for ion used cnt
+#include <linux/module.h>
+#endif /*VENDOR_EDIT*/
+
 atomic64_t page_sz_cnt = ATOMIC64_INIT(0);
 
 bool ion_buffer_fault_user_mappings(struct ion_buffer *buffer)
@@ -105,6 +111,18 @@ static void ion_buffer_add(struct ion_device *dev,
 	rb_link_node(&buffer->node, parent, p);
 	rb_insert_color(&buffer->node, &dev->buffers);
 }
+
+#ifdef VENDOR_EDIT
+/* Huacai.Zhou@PSW.BSP.Kernel.MM, 2018-06-26, add ion total used account*/
+static atomic_long_t ion_total_size;
+static bool ion_cnt_enable = true;
+unsigned long ion_total(void)
+{
+	if (!ion_cnt_enable)
+		return 0;
+	return (unsigned long)atomic_long_read(&ion_total_size);
+}
+#endif /*VENDOR_EDIT*/
 
 /* this function should only be called while dev->lock is held */
 static struct ion_buffer *ion_buffer_create(struct ion_heap *heap,
@@ -227,9 +245,15 @@ exit:
 	mutex_lock(&dev->buffer_lock);
 	ion_buffer_add(dev, buffer);
 	mutex_unlock(&dev->buffer_lock);
+
 	trace_ion_heap_grow(heap->name, len,
 			    atomic_long_read(&heap->total_allocated));
 	atomic_long_add(len, &heap->total_allocated);
+#ifdef VENDOR_EDIT
+/* Huacai.Zhou@PSW.BSP.Kernel.MM, 2018-06-26, add ion total used account*/
+	if (ion_cnt_enable)
+		atomic_long_add(buffer->size, &ion_total_size);
+#endif
 	return buffer;
 
 err1:
@@ -249,6 +273,11 @@ void ion_buffer_destroy(struct ion_buffer *buffer)
 	trace_ion_heap_shrink(buffer->heap->name,  buffer->size,
 			      atomic_long_read(&buffer->heap->total_allocated));
 	atomic_long_sub(buffer->size, &buffer->heap->total_allocated);
+#ifdef VENDOR_EDIT
+/* Huacai.Zhou@PSW.BSP.Kernel.MM, 2018-06-26, add ion total used account*/
+	if (ion_cnt_enable)
+		atomic_long_sub(buffer->size, &ion_total_size);
+#endif /*VENDOR_EDIT*/
 	buffer->heap->ops->free(buffer);
 	vfree(buffer->pages);
 	kfree(buffer);
@@ -2205,5 +2234,9 @@ file2buf_exit:
 	else
 		return ERR_PTR(-EINVAL);
 }
-
-/* ===================================== */
+#ifdef VENDOR_EDIT
+// wenbin.liu@PSW.BSP.MM, 2018/07/11
+// Add for ion show switch
+module_param_named(ion_cnt_enable, ion_cnt_enable, bool, S_IRUGO | S_IWUSR);
+#endif /*VENDOR_EDIT*/
+/* ============================================================================================= */
--- drivers/staging/android/ion/ion.h
+++ drivers/staging/android/ion/ion.h
@@ -79,7 +79,10 @@ struct ion_platform_data {
  */
 struct ion_client *ion_client_create(struct ion_device *dev,
 				     const char *name);
-
+#ifdef VENDOR_EDIT
+/* Huacai.Zhou@PSW.BSP.Kernel.MM, 2018-06-26, add ion total used account*/
+unsigned long ion_total(void);
+#endif /*VENDOR_EDIT*/
 /**
  * ion_client_destroy() -  free's a client and all it's handles
  * @client:	the client
@@ -108,6 +111,11 @@ void ion_client_destroy(struct ion_client *client);
 struct ion_handle *ion_alloc(struct ion_client *client, size_t len,
 			     size_t align, unsigned int heap_id_mask,
 			     unsigned int flags);
+
+#ifdef VENDOR_EDIT
+/* Huacai.Zhou@PSW.BSP.Kernel.MM, 2018-06-26, add ion total used account*/
+unsigned long ion_total(void);
+#endif /*VENDOR_EDIT*/
 
 /**
  * ion_free - free a handle
