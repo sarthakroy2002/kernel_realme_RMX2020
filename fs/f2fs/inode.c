ca1c2297d2c17260be34a01c3b0def94f9873db5
alk3pInjection <webmaster@raspii.tech>
2021-08-30 11:13:52 +0800

    treewide: Implement readahead filemap hooks
    Change-Id: I1a87bc9341cb5bfcfb56e9239fe2a53c3e602e0e

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -17,6 +17,7 @@
 #include "xattr.h"
 
 #include <trace/events/f2fs.h>
+#include <linux/file_map.h>
 
 void f2fs_mark_inode_dirty_sync(struct inode *inode, bool sync)
 {
@@ -462,6 +463,10 @@ static int do_read_inode(struct inode *inode)
 	stat_inc_compr_inode(inode);
 	stat_add_compr_blocks(inode, F2FS_I(inode)->i_compr_blocks);
 
+#ifdef CONFIG_FILE_MAP
+	inode->i_file_map = NULL;
+	file_map_entry_attach_unused(inode);
+#endif
 	return 0;
 }
 
