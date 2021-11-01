9d07539f13fe2abf1ecf509e1e92b161dc7bbccc
Cyber Knight <cyberknight755@gmail.com>
2021-11-02 04:25:49 +0800

    OplusKernelEnvConfig: nuke irrelevant warning spam
    * This just spams unnecessarily when any make operation is run
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- OplusKernelEnvConfig.mk
+++ OplusKernelEnvConfig.mk
@@ -112,7 +112,6 @@ OPLUS_FEATURE_APP_MONITOR \
 OPLUS_FEATURE_HDR10PLUS_CETIFICATION
 
 $(foreach myfeature,$(ALLOWED_MCROS),\
-         $(warning myfeature is $(myfeature)) \
          $(eval KBUILD_CFLAGS += -D$(myfeature)) \
          $(eval KBUILD_CPPFLAGS += -D$(myfeature)) \
          $(eval CFLAGS_KERNEL += -D$(myfeature)) \
