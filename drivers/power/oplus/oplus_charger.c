ceb590e0cd43a620ff7bd259de3daa915093648b
techyminati <sinha.aryan03@gmail.com>
2022-01-13 14:54:51 +0530

    drivers: power: oplus: Supress oplus_charger logging
    * oppo/oplus charger spams unwanted logs like:
    [ 153.116246] (0)[51:kworker/0:1][OPLUS_CHG][oplus_chg_print_log] CHGR[ 0 / 0 / 0 / 3 / 0 ], \x09\x09BAT[ 1 / 0 / 0 / 0 / 1 / 0 ], \x09\x09GAUGE[ 373 / 3785 / 3785 / 809 / -1 / 50 / 50 / 0 / 1634 ], STATUS[ 0x0 / 0 / 0 / 0 / 0 / 0x0 ], \x09\x09OTHER[ 0 / 1 / 0 / 0 / 0/ 0 ]
    
    * I've no idea why realme engineers like to use printk & enable debugging in production builds, so lets just supress this logging using pr_debug.
    
    [@techyminati] - Also Adapt it to oplus_charger from oppo_charger
    
    Change-Id: I2bab370c01e60d40f3c948d4b6b9c09ddcf39112
    Co-authored-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- drivers/power/oplus/oplus_charger.c
+++ drivers/power/oplus/oplus_charger.c
@@ -96,7 +96,7 @@ bool check_fastchg_quit = false;
 #define charger_xlog_printk(num, fmt, ...) \
 		do { \
 			if (enable_charger_log >= (int)num) { \
-				printk(KERN_NOTICE pr_fmt("[OPLUS_CHG][%s]"fmt), __func__, ##__VA_ARGS__); \
+				pr_debug(KERN_NOTICE pr_fmt("[OPLUS_CHG][%s]"fmt), __func__, ##__VA_ARGS__); \
 			} \
 		} while (0)
 
