e700581ccc586d9d198ec3c938a7fc69f901b67b
Sarthak Roy <sarthakroy2002@gmail.com>
2022-07-24 09:10:43 +0000

    drivers/input/touchscreen: Import from RMX2020-Q-OSS release
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- drivers/input/touchscreen/NT36672C/nt36xxx.h
+++ drivers/input/touchscreen/NT36672C/nt36xxx.h
@@ -68,7 +68,7 @@
 
 //---Touch info.---
 #define TOUCH_DEFAULT_MAX_WIDTH 1080
-#define TOUCH_DEFAULT_MAX_HEIGHT 2520
+#define TOUCH_DEFAULT_MAX_HEIGHT 2400
 #define TOUCH_MAX_FINGER_NUM 10
 #define TOUCH_KEY_NUM 0
 #if TOUCH_KEY_NUM > 0
@@ -89,10 +89,11 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 extern const uint16_t gesture_key_array[];
 #endif
 #define BOOT_UPDATE_FIRMWARE 1
-#define BOOT_UPDATE_FIRMWARE_NAME "novatek_ts_fw.bin"
+#define BOOT_UPDATE_FIRMWARE_NAME novatek_firmware
 #define MP_UPDATE_FIRMWARE_NAME   "novatek_ts_mp.bin"
 #define POINT_DATA_CHECKSUM 1
 #define POINT_DATA_CHECKSUM_LEN 65
+extern char novatek_firmware[25];
 
 //---ESD Protect.---
 #define NVT_TOUCH_ESD_PROTECT 0
