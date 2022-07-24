b4179b79f3c345596a788f6e7561155ce0aa173e
Sarthak Roy <sarthakroy2002@gmail.com>
2022-07-24 09:10:41 +0000

    drivers/base: Import from RMX2020-Q-OSS release
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- drivers/base/firmware_class.c
+++ drivers/base/firmware_class.c
@@ -195,6 +195,10 @@ static int __fw_state_check(struct fw_state *fw_st, enum fw_status status)
 #endif
 #define FW_OPT_NO_WARN	(1U << 3)
 #define FW_OPT_NOCACHE	(1U << 4)
+#ifdef VENDOR_EDIT
+//Tong.Han@Bsp.Group.Tp,2017-12-16,Add interface to get proper fw
+#define FW_OPT_COMPARE (1U << 5)
+#endif/*VENDOR_EDIT*/
 
 struct firmware_cache {
 	/* firmware_buf instance will be added into the below list */
@@ -367,11 +371,13 @@ static void fw_free_buf(struct firmware_buf *buf)
 /* direct firmware loading support */
 static char fw_path_para[256];
 static const char * const fw_path[] = {
+	"/data/misc/firmware/active/",
 	fw_path_para,
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
-	"/lib/firmware"
+	"/lib/firmware",
+	"/vendor/firmware"
 };
 
 /*
@@ -382,8 +388,11 @@ static const char * const fw_path[] = {
 module_param_string(path, fw_path_para, sizeof(fw_path_para), 0644);
 MODULE_PARM_DESC(path, "customized firmware image search path with a higher priority than default path");
 
-static int
-fw_get_filesystem_firmware(struct device *device, struct firmware_buf *buf)
+#ifdef VENDOR_EDIT
+//Wanghao@Bsp.Group.Tp,2018-02-13, Add to avoid direct pass encrypt tp firmware to driver
+static int fw_get_filesystem_firmware(struct device *device,
+				       struct firmware_buf *buf, unsigned int opt_flags)
+#endif
 {
 	loff_t size;
 	int i, len;
@@ -391,6 +400,14 @@ fw_get_filesystem_firmware(struct device *device, struct firmware_buf *buf)
 	char *path;
 	enum kernel_read_file_id id = READING_FIRMWARE;
 	size_t msize = INT_MAX;
+
+        #ifdef VENDOR_EDIT
+        //Wanghao@Bsp.Group.Tp,2018-02-13, Add to avoid direct pass encrypt tp firmware to driver
+        if(opt_flags & FW_OPT_COMPARE) {
+                pr_err("%s opt_flags get FW_OPT_COMPARE!\n", __func__);
+                return rc;
+        }
+	#endif/*VENDOR_EDIT*/
 
 	/* Already populated data member means we're loading into a buffer */
 	if (buf->data) {
@@ -1024,7 +1041,10 @@ static int _request_firmware_load(struct firmware_priv *fw_priv,
 	int retval = 0;
 	struct device *f_dev = &fw_priv->dev;
 	struct firmware_buf *buf = fw_priv->buf;
-
+    #ifdef VENDOR_EDIT
+	//Tong.Han@Bsp.Group.Tp,2017-12-16,Add interface to get proper fw
+	char *envp[2]={"FwUp=compare", NULL};
+	#endif/*VENDOR_EDIT*/
 	/* fall back on userspace loading */
 	if (!buf->data)
 		buf->is_paged_buf = true;
@@ -1045,7 +1065,16 @@ static int _request_firmware_load(struct firmware_priv *fw_priv,
 		buf->need_uevent = true;
 		dev_set_uevent_suppress(f_dev, false);
 		dev_dbg(f_dev, "firmware: requesting %s\n", buf->fw_id);
+        #ifdef VENDOR_EDIT
+		//Tong.Han@Bsp.Group.Tp,2017-12-16,Add interface to get proper fw
+		if (opt_flags & FW_OPT_COMPARE) {
+			kobject_uevent_env(&fw_priv->dev.kobj, KOBJ_CHANGE,envp);
+		} else {
+			kobject_uevent(&fw_priv->dev.kobj, KOBJ_ADD);
+		}
+		#else
 		kobject_uevent(&fw_priv->dev.kobj, KOBJ_ADD);
+        #endif/*VENDOR_EDIT*/
 	} else {
 		timeout = MAX_JIFFY_OFFSET;
 	}
@@ -1214,7 +1243,12 @@ _request_firmware(const struct firmware **firmware_p, const char *name,
 	if (ret <= 0) /* error or already assigned */
 		goto out;
 
-	ret = fw_get_filesystem_firmware(device, fw->priv);
+        #ifdef VENDOR_EDIT
+        //Wanghao@Bsp.Group.Tp,2018-02-13, Add to avoid direct pass encrypt tp firmware to driver
+		ret = fw_get_filesystem_firmware(device, fw->priv, opt_flags);
+		#else
+	    ret = fw_get_filesystem_firmware(device, fw->priv);
+		#endif
 	if (ret) {
 		if (!(opt_flags & FW_OPT_NO_WARN))
 			dev_warn(device,
@@ -1297,6 +1331,22 @@ int request_firmware_direct(const struct firmware **firmware_p,
 	return ret;
 }
 EXPORT_SYMBOL_GPL(request_firmware_direct);
+
+#ifdef VENDOR_EDIT
+//Tong.Han@Bsp.Group.Tp,2017-12-16,Add interface to get proper fw
+int request_firmware_select(const struct firmware **firmware_p, const char *name,
+		 struct device *device)
+{
+	int ret;
+	printk("%s:%d\n", __func__, __LINE__);
+	/* Need to pin this module until return */
+	__module_get(THIS_MODULE);
+	ret = _request_firmware(firmware_p, name, device, NULL, 0, FW_OPT_UEVENT | FW_OPT_FALLBACK | FW_OPT_COMPARE);
+	module_put(THIS_MODULE);
+	return ret;
+}
+EXPORT_SYMBOL(request_firmware_select);
+#endif/*VENDOR_EDIT*/
 
 /**
  * request_firmware_into_buf - load firmware into a previously allocated buffer
