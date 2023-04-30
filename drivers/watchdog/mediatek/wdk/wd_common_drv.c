2138347a18ea4fa7fb4f78ceb976b24b74ca1382
Sarthak Roy <sarthakroy2002@gmail.com>
2023-04-30 15:47:30 +0000

    drivers: watchdog: mtk: wdk: Conditionally call dump_wdk_bind_info
    * Following https://github.com/sarthakroy2002/kernel_realme_wasabi/commit/5220155a2a66ad8589fde567894424551e44543f
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>

--- drivers/watchdog/mediatek/wdk/wd_common_drv.c
+++ drivers/watchdog/mediatek/wdk/wd_common_drv.c
@@ -557,8 +557,10 @@ static void kwdt_process_kick(int local_bit, int cpu,
 	else
 		printk_deferred("%s", msg_buf);
 
+#ifdef CONFIG_MTK_TICK_BROADCAST_AEE_DUMP
 	if (dump_timeout)
 		dump_wdk_bind_info();
+#endif
 
 #ifdef CONFIG_LOCAL_WDT
 	printk_deferred("[wdk] cpu:%d, kick local wdt,RT[%lld]\n",
