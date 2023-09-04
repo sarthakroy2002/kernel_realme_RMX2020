e7cd5a1808d3506ce8f23ceadec207a6093b82a2
Sarthak Roy <sarthakroy2002@gmail.com>
2023-09-04 21:50:49 +0530

    drivers/mtk/typec: tcpc: Use __maybe_unused
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Iffd551383e60acea7b64e70019a27d83d162b23d

--- drivers/misc/mediatek/typec/tcpc/tcpc_rt1711h.c
+++ drivers/misc/mediatek/typec/tcpc/tcpc_rt1711h.c
@@ -800,7 +800,7 @@ static inline int rt1711_fault_status_vconn_ov(struct tcpc_device *tcpc)
 
 int rt1711_fault_status_clear(struct tcpc_device *tcpc, uint8_t status)
 {
-	int ret;
+	int __maybe_unused ret;
 
 	if (status & TCPC_V10_REG_FAULT_STATUS_VCONN_OV)
 		ret = rt1711_fault_status_vconn_ov(tcpc);
