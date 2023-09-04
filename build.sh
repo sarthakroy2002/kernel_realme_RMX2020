6bfd079b9f669cab65136a733d11ad1c3654d468
Sarthak Roy <sarthakroy2002@gmail.com>
2023-09-04 18:58:55 +0530

    build script: Refactor
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Iff48dbe3b0609eecd69d2dc66b4c823a3864bc6d

--- build.sh
+++ build.sh
@@ -2,45 +2,45 @@
 
 function compile() 
 {
-
+rm -rf AnyKernel
 source ~/.bashrc && source ~/.profile
 export LC_ALL=C && export USE_CCACHE=1
-ccache -M 100G
 export ARCH=arm64
 export KBUILD_BUILD_HOST=neolit
 export KBUILD_BUILD_USER="sarthakroy2002"
-git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
-git clone --depth=1 https://github.com/sarthakroy2002/prebuilts_gcc_linux-x86_aarch64_aarch64-linaro-7 los-4.9-64
-git clone --depth=1 https://github.com/sarthakroy2002/linaro_arm-linux-gnueabihf-7.5 los-4.9-32
+mkdir clang && cd clang
+bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) -S=latest
+bash <(curl -s https://raw.githubusercontent.com/Neutron-Toolchains/antman/main/antman) --patch=glibc
+cd ..
 
 [ -d "out" ] && rm -rf out || mkdir -p out
 
 make O=out ARCH=arm64 RMX2020_defconfig
 
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/los-4.9-32/bin:${PATH}:${PWD}/los-4.9-64/bin:${PATH}" \
+PATH="${PWD}/clang/bin:${PATH}" \
 make -j$(nproc --all) O=out \
                       ARCH=arm64 \
                       CC="clang" \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/los-4.9-64/bin/aarch64-linux-gnu-" \
-                      CROSS_COMPILE_ARM32="${PWD}/los-4.9-32/bin/arm-linux-gnueabihf-" \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                      LLVM=1 \
                       LD=ld.lld \
                       AS=llvm-as \
 		              AR=llvm-ar \
 			          NM=llvm-nm \
 			          OBJCOPY=llvm-objcopy \
+                      OBJDUMP=llvm-objdump \
+                      STRIP=llvm-strip \
                       CONFIG_NO_ERROR_ON_MISMATCH=y
 }
 
-function zupload()
+function zipping()
 {
 git clone --depth=1 https://github.com/sarthakroy2002/AnyKernel3.git AnyKernel
 cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 cd AnyKernel
 zip -r9 Test-OSS-KERNEL-RMX2020-NEOLIT.zip *
-curl -sL https://git.io/file-transfer | sh
-./transfer wet Test-OSS-KERNEL-RMX2020-NEOLIT.zip
 }
 
 compile
-zupload
+zipping
