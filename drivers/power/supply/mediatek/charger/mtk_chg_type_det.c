aa730001c08a47f94e7f496fc2bb04dffe318f0a
Sarthak Roy <sarthakroy2002@gmail.com>
2023-09-20 21:37:43 +0530

    drivers: power/mediatek: Shut `pmic_chrdet_status` spam
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I6889051d8dbfeb7996dbc5247f849adf4b568cb8

--- drivers/power/supply/mediatek/charger/mtk_chg_type_det.c
+++ drivers/power/supply/mediatek/charger/mtk_chg_type_det.c
@@ -997,17 +997,17 @@ bool pmic_chrdet_status(void)
 		return true;
 #else
 		if (mt_usb_is_device()) {
-			pr_err("[%s],Charger exist and USB is not host\n",__func__);
+			//pr_err("[%s],Charger exist and USB is not host\n",__func__);
 			notify_charger_status(true);
 			return true;
 		} else {
-			pr_err("[%s],Charger exist but USB is host, now skip\n",__func__);
+			//pr_err("[%s],Charger exist but USB is host, now skip\n",__func__);
 			notify_charger_status(false);
 			return false;
 		}
 #endif
 	}
-	pr_notice("%s: No charger\n", __func__);
+	//pr_notice("%s: No charger\n", __func__);
 #ifdef VENDOR_EDIT
 /* Yichun.Chen  PSW.BSP.CHG  2019-08-11  for aging issue */
 	notify_charger_status(false);
