0d92c524fe08ef7163a4696f9d1be85fe00f8838
Sarthak Roy <sarthakroy2002@gmail.com>
2023-12-02 11:02:24 +0530

    treewide: Nuke more OPLUS crap
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I74ed5de81b2b5c746fb812bf4a593d9efe5fa185

--- drivers/misc/mediatek/aee/aed/aed-main.c
+++ drivers/misc/mediatek/aee/aed/aed-main.c
@@ -956,13 +956,7 @@ static void ee_gen_ind_msg(struct aed_eerec *eerec)
 		return;
 
 	rep_msg->cmdType = AE_IND;
-//#ifndef OPLUS_FEATURE_ENABLE_MODEM_DB
-//CaiWenlong@NETWORK.RF.CFG, 2016/10/10,
-//Modify for enable create DB file when modem crash on release build
-    //rep_msg->cmdId = AE_IND_EXP_RAISED;
-//#else  /* OPLUS_FEATURE_ENABLE_MODEM_DB */
     rep_msg->cmdId = AE_IND_FATAL_RAISED;
-//#endif /* OPLUS_FEATURE_ENABLE_MODEM_DB */
 	rep_msg->arg = AE_EE;
 	rep_msg->len = 0;
 	rep_msg->dbOption = eerec->db_opt;
@@ -2412,15 +2406,6 @@ static void external_exception(const char *assert_type, const int *log,
 	struct timeval tv = { 0 };
 	char trigger_time[60];
 
-//#ifdef OPLUS_FEATURE_ENABLE_MODEM_DB
-//CaiWenlong@NETWORK.RF.CFG, 2016/10/10,
-//Delete for enable create DB file when modem crash on release build
-/*
-	if ((aee_mode >= AEE_MODE_CUSTOMER_USER) &&
-		(aee_force_exp == AEE_FORCE_EXP_NOT_SET))
-		return;
-*/
-//#endif /* OPLUS_FEATURE_ENABLE_MODEM_DB */
 	eerec = kzalloc(sizeof(struct aed_eerec), GFP_ATOMIC);
 	if (eerec == NULL) {
 		return;
