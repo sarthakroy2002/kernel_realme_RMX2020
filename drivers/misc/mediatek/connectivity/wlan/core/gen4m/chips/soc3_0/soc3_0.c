bf492266beea1a8c4000fb197da6f085fc757357
Sarthak Roy <sarthakroy2002@gmail.com>
2023-12-02 11:05:04 +0530

    drivers: mtk/connectivity: Shut SEM-CTRL ROM patch message
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: I84e56bd811cc04c3445e633554322fdcb1619883

--- drivers/misc/mediatek/connectivity/wlan/core/gen4m/chips/soc3_0/soc3_0.c
+++ drivers/misc/mediatek/connectivity/wlan/core/gen4m/chips/soc3_0/soc3_0.c
@@ -3332,7 +3332,6 @@ uint32_t soc3_0_wlanPowerOnDownload(
 		}
 
 #if (CFG_ROM_PATCH_NO_SEM_CTRL == 0)
-#pragma message("ROM code supports SEM-CTRL for ROM patch download")
 		if (wlanPatchIsDownloaded(prAdapter)) {
 			kalFirmwareImageUnmapping(prAdapter->prGlueInfo, NULL,
 						  prFwBuffer);
@@ -3340,8 +3339,6 @@ uint32_t soc3_0_wlanPowerOnDownload(
 				"No need to download patch\n");
 			return WLAN_STATUS_SUCCESS;
 		}
-#else
-#pragma message("ROM code supports no SEM-CTRL for ROM patch download")
 #endif
 
 		/* Patch DL */
