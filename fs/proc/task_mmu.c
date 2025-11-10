27c7792006d49024e7250ccb94a7334e9152c9cb
crypto-zh1901 <ganznanta@gmail.com>
2025-11-10 13:41:47 +0000

    Fix

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -376,19 +376,19 @@ show_map_vma(struct seq_file *m, struct vm_area_struct *vma)
 #ifdef CONFIG_KSU_SUSFS_SUS_MAP
 		if (unlikely(inode->i_mapping->flags & BIT_SUS_MAPS) && susfs_is_current_proc_umounted()) {
 			seq_setwidth(m, 25 + sizeof(void *) * 6 - 1);
-			seq_put_hex_ll(m, NULL, vma->vm_start, 8);
-			seq_put_hex_ll(m, "-", vma->vm_end, 8);
-			seq_putc(m, ' ');
-			seq_putc(m, '-');
-			seq_putc(m, '-');
-			seq_putc(m, '-');
-			seq_putc(m, 'p');
-			seq_put_hex_ll(m, " ", pgoff, 8);
-			seq_put_hex_ll(m, " ", MAJOR(dev), 2);
-			seq_put_hex_ll(m, ":", MINOR(dev), 2);
-			seq_put_decimal_ull(m, " ", ino);
-			seq_putc(m, ' ');
-			goto done;
+			seq_put_hex_ll_width(m, NULL, vma->vm_start, 8);
+                        seq_put_hex_ll_width(m, "-", vma->vm_end, 8);
+                        seq_putc(m, ' ');
+                        seq_putc(m, '-');
+                        seq_putc(m, '-');
+                        seq_putc(m, '-');
+                        seq_putc(m, 'p');
+                        seq_put_hex_ll_width(m, " ", pgoff, 8);
+                        seq_put_hex_ll_width(m, " ", MAJOR(dev), 2);
+                        seq_put_hex_ll_width(m, ":", MINOR(dev), 2);
+                        seq_put_decimal_ull(m, " ", ino);
+                        seq_putc(m, ' ');
+                        goto done;
 		}
 #endif
 #ifdef CONFIG_KSU_SUSFS_SUS_KSTAT
