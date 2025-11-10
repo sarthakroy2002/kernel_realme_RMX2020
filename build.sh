39141e8cc979061bc128b7a65b6a51a02aa836d4
crypto-zh1901 <ganznanta@gmail.com>
2025-11-10 17:04:41 +0000

    3

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-SECONDS=0
-set -e
 
-# Set kernel path
-KERNEL_PATH=out/arch/arm64/boot
-
-# Set kernel name
-BUILD_TYPE="Stable"
-DATE="$(TZ=Asia/Jakarta date +%Y%m%d%H%M%S)"
-KERNEL_NAME="Ksu-Next${BUILD_TYPE}-${DATE}.zip"
+function compile() 
+{
+rm -rf AnyKernel
+source ~/.bashrc && source ~/.profile
+export LC_ALL=C && export USE_CCACHE=1
+export ARCH=arm64
+export KBUILD_BUILD_HOST=codespace
+export KBUILD_BUILD_USER=anonim
 
 # Clone SukiSU repo
 if [ ! -d "KernelSU" ]; then curl -LSs "https://raw.githubusercontent.com/kernel-build-from-rainyland/KernelSU-Next/next/kernel/setup.sh" | bash -s next; fi
@@ -19,19 +18,13 @@ function KERNEL_COMPILE() {
 		sudo apt update -y && sudo apt upgrade -y && sudo apt install nano bc ccache bison ca-certificates curl flex gcc git libc6-dev libssl-dev openssl python-is-python3 ssh wget zip zstd sudo make clang gcc-arm-linux-gnueabi software-properties-common build-essential libarchive-tools gcc-aarch64-linux-gnu -y && sudo apt install build-essential -y && sudo apt install libssl-dev libffi-dev libncurses5-dev zlib1g zlib1g-dev libreadline-dev libbz2-dev libsqlite3-dev make gcc -y && sudo apt install pigz -y && sudo apt install python2 -y && sudo apt install python3 -y && sudo apt install cpio -y && sudo apt install lld -y && sudo apt install llvm -y && sudo apt-get install g++-aarch64-linux-gnu -y && sudo apt install libelf-dev -y && sudo apt install neofetch -y && neofetch
 	fi
 
-	# Set environment variables
-	export USE_CCACHE=1
-	export KBUILD_BUILD_HOST=codespace
-	export KBUILD_BUILD_USER=anonim
-
-	# Create output directory and do a clean build
-	rm -rf out && mkdir -p out
-
-	# Download clang if not present
+# Download clang if not present
 	if [[ ! -d "clang" ]]; then mkdir -p clang
 		wget https://github.com/Impqxr/aosp_clang_ci/releases/download/13289611/clang-13289611-linux-x86.tar.xz -O clang.tar.gz
 		tar -xf clang.tar.gz -C clang && if [ -d clang/clang-* ]; then mv clang/clang-*/* clang; fi && rm -rf clang.tar.gz
 	fi
+
+       [ -d "out" ] && rm -rf out || mkdir -p out
 
 	# Add clang bin directory to PATH
 	export PATH="${PWD}/clang/bin:$PATH"
@@ -44,20 +37,15 @@ function KERNEL_COMPILE() {
 }
 
 function KERNEL_RESULT() {
-        # Check is build is successful
-	if [ ! -f ${KERNEL_PATH}/Image ]; then
-		exit 1
-	fi
-
-        # Copying image
-	cp ${KERNEL_PATH}/Image.gz-dtb AnyKernel/
-
-	# Create AnyKernel
+	# Create anykernel
 	rm -rf AnyKernel
-	git clone https://github.com/muhammmadnantaa-hub/AnyKernel.git AnyKernel
+	git clone https://github.com/sarthakroy2002/AnyKernel3.git AnyKernel
+
+	# Cop
+        cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 
 	# Created zip kernel
-	cd AnyKernel && zip -r9 "${KERNEL_NAME}" *
+	cd AnyKernel && zip -r9 ksu-next.zip *
 
 	# Upload kernel
 	RESPONSE=$(curl -s -F "file=@${KERNEL_NAME}" "https://store1.gofile.io/contents/uploadfile" \
